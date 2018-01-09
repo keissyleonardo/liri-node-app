003a71b07636e0ce9a569fc09168cb8743efd1d6
Keissy <keissy@lifecellskincare.com>
2018-01-09 13:15:09 -0500

    First commit

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '7ske37OjgmFY6cbZCgsZ3JkUs ',
+  consumer_secret: 'baed6o8EiZVuAAyj6zRbB0s2WMh8JpMj0KXOw8TgAN6xkMwMZr',
+  access_token_key: '950786806097829889-xPm1tp3nT75oND5PygLJVtVSQltlRTe',
+  access_token_secret: '6fDfbgZNEHaGRxSSdsV03ttmFrMUtebaxVTgEmijUm5bE',
+}
